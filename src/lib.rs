e9053ca6ed3a8c36da44550a9606e4bfa1bc8334
wesley-lewis <lewis.wes.23@gmail.com>
2023-08-22 13:13:24 +0530

    Name element parsing done

---
+++ src/lib.rs
@@ -0,0 +1,110 @@
+#[allow(warnings)]
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Element {
+    name: String,
+    attributes: Vec<(String, String)>,
+    children: Vec<Element>,
+}
+
+type ParseResult<'a, Output> = Result<(&'a str, Output), &'a str>;
+
+#[allow(dead_code)]
+// Fn(&str) -> Result<(&str, Element), Error>
+fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
+    match input.chars().next() {
+        Some('a') => Ok((&input['a'.len_utf8()..], ())),
+        _ => Err(input),
+    }
+}
+
+#[allow(dead_code)]
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphabetic() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
+}
+
+fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| match parser1(input) {
+        Ok((next_input, result1)) => match parser2(next_input) {
+            Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
+
+#[allow(dead_code)]
+fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+    move |input| match input.get(0..expected.len()) {
+        Some(next) if next == expected => Ok((&input[expected.len()..], ())),
+        _ => Err(input),
+    }
+}
+
+#[allow(dead_code)]
+fn map<P, F, A, B>(parser:P,  map_fn: F) -> impl Fn(&str) -> Result<(&str, B), &str> 
+where 
+    P: Fn(&str) -> Result<(&str, A), &str>,
+    F: Fn(A) -> B,
+{
+    move | input |  
+        parser(input)
+        .map(|(next_input, result)| (next_input, map_fn(result)))
+}
+
+#[test]
+fn literal_parser() {
+    let parse_joe = match_literal("Hello Joe!");
+    assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
+    assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"),);
+}
+
+#[test]
+fn identifier_parser() {
+    assert_eq!(
+        Ok(("", "i-am-an-identifier".to_string())),
+        identifier("i-am-an-identifier")
+    );
+
+    assert_eq!(
+        Ok((" entirely an identifier", "not".to_string())),
+        identifier("not entirely an identifier")
+    );
+
+    assert_eq!(
+        Err("!not at all an identifier"),
+        identifier("!not at all an identifier"),
+    );
+}
+
+#[test]
+fn pair_combinator() {
+    let tag_opener = pair(match_literal("<"), identifier);
+    assert_eq!(
+        Ok(("/>", ((), "my-first-element".to_string()))),
+        tag_opener("<my-first-element/>")
+        );
+
+    assert_eq!(Err("oops"), tag_opener("oops"));
+    assert_eq!(Err("!oops"), tag_opener("<!oops"));
+}
